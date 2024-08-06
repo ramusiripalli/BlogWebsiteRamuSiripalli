bc02134ebed52174d8fb48be9076a91b886f93b0
ramu siripalli <ramsiripalli2772@gmail.com>
2024-08-06 11:16:36 +0530

    Set up Basic Routes using Hono
    Set up Basic Routes using Hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,26 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
+  return c.text('Hello Ramu')
+})
+
+app.post('/api/v1/user/signup', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello /api/v1/blog!')
+})
+
+
 export default app
+
