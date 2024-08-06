1b9320937009e8dbec9adbbc3f4282b47ab870b0
ramu siripalli <ramsiripalli2772@gmail.com>
2024-08-06 14:46:42 +0530

    added signup route to application
    where i have created and added into database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,33 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL : string;
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Ramu')
-})
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json()
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello Hono!')
+try{
+  await prisma.user.create({
+    data : {
+     email: body.email,
+     password: body.password,
+     name: body.name
+  
+    }
+  })
+  return c.text('Hello welcome to the signup route')
+} catch(e){
+  c.status(411);
+  return c.text('User already exists with this email')
+}
 })
 
 app.post('/api/v1/user/signin', (c) => {
@@ -23,26 +43,5 @@ app.put('/api/v1/blog', (c) => {
   return c.text('Hello /api/v1/blog!')
 })
 
-
 export default app
-
-
-
-/*
-
-
-backend will pick database from wrangler.toml 
-
-
-
-
-
-prisma connection pool url : which will be used in node js application
-
-DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZGE0MzQ3YjctMzBjOC00MGNlLTg0ZDktZDliYjUxOGQ3YWE2IiwidGVuYW50X2lkIjoiMTlkMTZmYTljZjdlZDUxOGQ0YmQzNmRiYTRiMjMyNzg2NjA0MmQ3Nzg2OWZkMDgxNWQxYzM0NzBkZWE2OTE4YSIsImludGVybmFsX3NlY3JldCI6IjI1YTQ5Zjg3LWVkMGQtNDhlZi1hNzk5LTU0NDAxMTkxYWY5MyJ9.1UCXl4TVq22KcPV_aJ2Yhxq0ffmk9w1CrjMge3u1E50"
-
-postgres database Url : which will be pasted in connection pool
-postgresql://RamuPostgres_owner:cNRxIHvZQ0O8@ep-hidden-haze-a1reppqo.ap-southeast-1.aws.neon.tech/RamuPostgres?sslmode=require 
-
-*/
 
