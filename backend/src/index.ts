a3838f7218170f82f0990f4171a39bdfa39c7c48
ramu siripalli <ramsiripalli2772@gmail.com>
2024-08-08 19:17:13 +0530

    added user and blogs Endpoint
    added user and blogs Endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+
 
 const app = new Hono<{
   Bindings: {
@@ -10,69 +10,12 @@ const app = new Hono<{
   }
 }>()
 
-app.post('/api/v1/user/signup', async (c) => {
-  const body = await c.req.json()
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-
-try{
-const user =   await prisma.user.create({
-    data : {
-     email: body.email,
-     password: body.password,
-     name: body.name
-  
-    }
-  })
-  const jwt = await sign({
-    id: user.id
-  }, c.env.JWT_SECRET)
-  return c.text(jwt)
-} catch(e){
-  c.status(411);
-  return c.text('User already exists with this email')
-}
-})
-
-app.post('/api/v1/user/signin', async(c) => {
-
-  const body = await c.req.json()
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-
-try{
-const user =   await prisma.user.findFirst({
-    where : {
-     email: body.email,
-     password: body.password,
- 
-  
-    }
-  })
-  if(!user){
-    c.status(403);
-    return c.text('Invalid');
-  }
-  const jwt = await sign({
-    id: user.id
-  }, c.env.JWT_SECRET)
-  return c.text(jwt)
-} catch(e){
-  c.status(411);
-  return c.text('User already exists with this email')
-}
-})
-
-app.get('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter)
 
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello /api/v1/blog!')
-})
+
+
 
 export default app
 
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,64 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+
+export const userRouter = new Hono<{
+    Bindings: {
+      DATABASE_URL : string;
+      JWT_SECRET : string
+    }
+  }>();
+  
+userRouter.post('/signup', async (c) => {
+    const body = await c.req.json()
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  try{
+  const user = await prisma.user.create({
+      data : {
+       email: body.email,
+       password: body.password,
+       name: body.name
+      }
+    })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+    return c.text("jwt------------>" + jwt)
+  } catch(e){
+    c.status(411);
+    return c.text('User already exists with this email')
+  }
+  })
+  
+  userRouter.post('/signin', async(c) => {
+    const body = await c.req.json()
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  try{
+  const user =   await prisma.user.findFirst({
+      where : {
+       email: body.email,
+       password: body.password,
+      }
+    })
+    if(!user){
+      c.status(403);
+      return c.json({
+        message: "Incorrect credentails"
+      });
+    }
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+    return c.text(jwt)
+  } catch(e){
+    c.status(411);
+    return c.text('User already exists with this email')
+  }
+  })
