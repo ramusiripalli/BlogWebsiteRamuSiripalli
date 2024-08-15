231281c49f0b519153dc670a3d5cc077af569d13
ramu siripalli <ramsiripalli2772@gmail.com>
2024-08-15 12:47:54 +0530

    Front End Created Basic Routes
    Front End Created Basic Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   
-      <div>
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        </div>
-     
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,13 @@
+import { Quote } from "../components/Quote"
+
+export const Signup = () => {
+
+    return <div className="grid grid-cols-2">
+        <div>
+
+        </div>
+        <div className="invisible md:visible">
+        <Quote />
+        </div>
+    </div>
+}
